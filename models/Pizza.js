8cf5a24e17d40267e8d9dbe5435f490edf2d6c64
Baron C <cannonbaron@gmail.com>
2021-08-10 20:45:13 -0500

    adding controller, model and routes for pizza hunt using mongoose

---
+++ models/Pizza.js
@@ -0,0 +1,25 @@
+//SCHEMA for Pizza Model
+
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+  pizzaName: {
+    type: String
+  },
+  createdBy: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  size: {
+    type: String,
+    default: 'Large'
+  },
+  toppings: []
+});
+
+const Pizza = model('Pizza', PizzaSchema);
+
+module.exports = Pizza;
---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,25 @@
+//API specific routes
+const router = require('express').Router();
+//Implement Controller Methods
+const {
+  getAllPizza,
+  getPizzaById,
+  createPizza,
+  updatePizza,
+  deletePizza
+} = require('../../controllers/pizza-controller');
+
+// /api/pizzas
+router
+  .route('/')
+  .get(getAllPizza)
+  .post(createPizza);
+
+// /api/pizzas/:id
+router
+  .route('/:id')
+  .get(getPizzaById)
+  .put(updatePizza)
+  .delete(deletePizza);
+
+module.exports = router;
